526fab6b4773986142283001f78da072ca92201f
孙源辉 <sunyuanhui@wisonic.cn>
2020-01-04 00:10:50 +0800

    没找到方法可以在jest中输出日志

--- js/two_sum/two_sum.test.js
+++ js/two_sum/two_sum.test.js
@@ -1,5 +1,13 @@
 const twoSum = require('./two_sum');
 
-test('two sum', () => {
-  expect(twoSum(1, 2)).toBe(1);
+test('two sum 1', () => {
+  const nums = [2, 7, 11, 15];
+  const target = 9;
+  expect(twoSum(nums, target)).toStrictEqual([0, 1]);
+});
+
+test('two sum 2', () => {
+  const nums = [3, 3];
+  const target = 6;
+  expect(twoSum(nums, target)).toStrictEqual([0, 1]);
 });
