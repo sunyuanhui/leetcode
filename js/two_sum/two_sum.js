58b91905e5224f01ae9c5f1e220a92ea42437bf0
孙源辉 <sunyuanhui@wisonic.cn>
2020-01-05 15:21:20 +0800

    完成两数之和

--- js/two_sum/two_sum.js
+++ js/two_sum/two_sum.js
@@ -6,29 +6,17 @@
 var twoSum = function(nums, target) {
     let tmp = {};
     nums.forEach((num, index) => {
-        if (tmp[num]) {
-            tmp[num] = [...tmp[num], index];
-            console.log(tmp + '\n');
-        } else {
-            tmp[num] = index;
-        }
+        let numIndex = tmp[num];
+        tmp[num] = numIndex === undefined ? [index] : [...numIndex, index];
     })
     
     for (let num1 in tmp) {
-        let num2;
-        if (num1 instanceof Array) {
-            num2 = target - num1[0];
-            console.log(num2 + '\n');
-        } else {
-            num2 = target - num1;
-        }
-
+        let num2 = target - num1;
         if (tmp[num2]) {
-            if (tmp[num2] instanceof Array) {
-                console.log(tmp[num2][0] + '\n');
-                return [tmp[num1], tmp[num2][0]];
+            if (num1 == num2) {
+                return [tmp[num1][0], tmp[num1][1]];
             }
-            return [tmp[num1], tmp[num2]];
+            return [tmp[num1][0], tmp[num2][0]];
         }
     }
     return [];
