107f20eef764630c4e8fbb3fda4ca1cb3843904f
孙源辉 <sunyuanhui@wisonic.cn>
2020-01-07 22:31:52 +0800

    优化算法速度提升到300ms+，不过还是不够优化

--- js/5. Longest Palindromic Substring/LongestPalindromicSubstring.js
+++ js/5. Longest Palindromic Substring/LongestPalindromicSubstring.js
@@ -5,31 +5,44 @@
 var longestPalindrome = function(s) {
     let first = 0;
     let last = 0;
-    for (let head = 0; head < s.length; head++) {
-        if ((s.length - 1 - head) <= (last - first)) {
-            break;
-        }
-
-        for (let tail = s.length - 1; tail > head; tail--) {
-            if ((tail - head) <= (last - first)) {
-                break;
-            }
-
-            for (let i = head, j = tail; i <= j; i++, j--) {
-                if (s[i] !== s[j]) {
-                    break;
-                }
-
-                if (i === j || (i + 1 === j)) {
-                    if ((tail - head) > (last - first)) {
-                        first = head;
-                        last = tail;
-                    }
-                    break;
-                }
-            }
-        }
+    let alphabet = {};
+    for (let i = 0; i < s.length; i++) {
+        let l = s[i];
+        alphabet[l] = alphabet[l] ? [...alphabet[l], i] : [i];
     }
+
+    if (Object.keys(alphabet).length === 1) {
+        return s;
+    }
+
+    Object.keys(alphabet).forEach((letter) => {
+        let indexs = alphabet[letter];
+
+        for (let headIndex = 0; headIndex < indexs.length; headIndex++) {
+            let head = indexs[headIndex];
+
+            for (let tailIndex = indexs.length - 1; tailIndex > 0; tailIndex--) {
+                let tail = indexs[tailIndex];
+                if ((tail - head) <= (last - first)) {
+                    break;
+                }
+
+                for (let i = head, j = tail; i <= j; i++ , j--) {
+                    if (s[i] !== s[j]) {
+                        break;
+                    }
+
+                    if (i === j || (i + 1 === j)) {
+                        if ((tail - head) > (last - first)) {
+                            first = head;
+                            last = tail;
+                        }
+                        break;
+                    }
+                }
+            }
+        }
+    })
     return s.substring(first, last + 1);
 };
 module.exports = longestPalindrome;
