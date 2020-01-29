abddbf8835b275dd3185d79b904e4e3479bcd20a
孙源辉 <sunyuanhui@wisonic.cn>
2020-01-30 00:01:59 +0800

    完成 Z 字形变换

--- js/6. ZigZag Conversion/ZigZagConversion.js
+++ js/6. ZigZag Conversion/ZigZagConversion.js
@@ -4,9 +4,44 @@
  * @return {string}
  */
 var convert = function(s, numRows) {
-    const numsInGroup = numRows * 2 - 2;
-    const groupNums = s.length / numsInGroup;
-    groupNums += (s.length % numsInGroup == 0) ? 0 : 1;
-    return 'PAHNAPLSIIGYIR';
+    let numCharsInGroup = numRows * 2 - 2;
+    if (numCharsInGroup === 0) {
+        numCharsInGroup = numRows;
+    }
+    const numGroups = Math.ceil(s.length / numCharsInGroup);
+    let numCharsInLastGroup = s.length % numCharsInGroup;
+    if (numCharsInLastGroup === 0) {
+        numCharsInLastGroup = numCharsInGroup;
+    }
+
+    let result = [];
+    for (let i = 0; i < numGroups; i++) {
+        result.push(s[i * numCharsInGroup]);
+    }
+
+    for (let row = 1; row < numRows - 1; row++) {
+        let numCharsInRow = numGroups * 2;
+        if (numCharsInLastGroup <= row) {
+            numCharsInRow -= 2;
+        } else if (numCharsInGroup - numCharsInLastGroup >= row) {
+            numCharsInRow -= 1;
+        }
+
+        for (let i = 0; i < numCharsInRow; i++) {
+            const charGroup = Math.floor(i / 2);
+            const index = numCharsInGroup * charGroup + 
+                (i % 2 === 1 ? (numCharsInGroup - row) : row);
+            result.push(s[index]);
+        }
+    }
+
+    if (numRows > 1) {
+        const numCharsInLastRow = numGroups - (numCharsInLastGroup >= numRows ? 0 : 1);
+        for (let i = 0; i < numCharsInLastRow; i++) {
+            result.push(s[(numRows - 1) + numCharsInGroup * i]);
+        }
+    }
+
+    return result.join('');
 };
 module.exports = convert;
--- js/6. ZigZag Conversion/ZigZagConversion.test.js
+++ js/6. ZigZag Conversion/ZigZagConversion.test.js
@@ -11,3 +11,21 @@ test('zig zag conversion 2', () => {
   const numRows = 4;
   expect(convert(s, numRows)).toBe('PINALSIGYAHRPI');
 });
+
+test('zig zag conversion 3', () => {
+  const s = 'LEETCODEISHIRING';
+  const numRows = 3;
+  expect(convert(s, numRows)).toBe('LCIRETOESIIGEDHN');
+});
+
+test('zig zag conversion 4', () => {
+  const s = 'LEETCODEISHIRING';
+  const numRows = 4;
+  expect(convert(s, numRows)).toBe('LDREOEIIECIHNTSG');
+});
+
+test('zig zag conversion 5', () => {
+  const s = 'A';
+  const numRows = 1;
+  expect(convert(s, numRows)).toBe('A');
+});
