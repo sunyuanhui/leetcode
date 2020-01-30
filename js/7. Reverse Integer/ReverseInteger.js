124c82a1c0eb241b7e2287e6a94d93838d0311eb
孙源辉 <sunyuanhui@wisonic.cn>
2020-01-30 16:23:21 +0800

    完成 7. 整数反转

---
+++ js/7. Reverse Integer/ReverseInteger.js
@@ -0,0 +1,24 @@
+﻿/**
+ * @param {number} x
+ * @return {number}
+ */
+var reverse = function(x) {
+    const MAX_VALUE = 2147483647;
+    const MIN_VALUE = -2147483648;
+    if (x > MAX_VALUE || x < MIN_VALUE) {
+        return 0;
+    }
+
+    let sign = x >= 0 ? 1 : -1;
+    x = x * sign;
+    let result = 0;
+    while (x !== 0) {
+        result = result * 10 + x % 10;
+        x = (x - x % 10) / 10;
+        if (result > MAX_VALUE || result < MIN_VALUE) {
+            return 0;
+        }
+    }
+    return result * sign;
+};
+module.exports = reverse;
---
+++ js/7. Reverse Integer/ReverseInteger.test.js
@@ -0,0 +1,31 @@
+﻿const reverse = require('./ReverseInteger');
+
+test('reverse integer 1', () => {
+  const num = 123;
+  expect(reverse(num)).toBe(321);
+});
+
+test('reverse integer 2', () => {
+  const num = -123;
+  expect(reverse(num)).toBe(-321);
+});
+
+test('reverse integer 3', () => {
+  const num = 120;
+  expect(reverse(num)).toBe(21);
+});
+
+test('reverse integer 4', () => {
+  const num = 2147483648;
+  expect(reverse(num)).toBe(0);
+});
+
+test('reverse integer 5', () => {
+  const num = -2147483649;
+  expect(reverse(num)).toBe(0);
+});
+
+test('reverse integer 6', () => {
+  const num = 1534236469;
+  expect(reverse(num)).toBe(0);
+});
