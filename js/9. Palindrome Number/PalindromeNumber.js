2f792532d7ce31ce014bdc7af69cb36121296423
孙源辉 <sunyuanhui@wisonic.cn>
2020-01-31 23:02:35 +0800

    完成 9. 回文数

---
+++ js/9. Palindrome Number/PalindromeNumber.js
@@ -0,0 +1,19 @@
+﻿/**
+ * @param {number} x
+ * @return {boolean}
+ */
+var isPalindrome = function(x) {
+    if (x < 0) {
+        return false;
+    }
+
+    let t = x;
+    let y = 0;
+    while (t !== 0) {
+        y = y * 10 + t % 10;
+        t = (t - t % 10) / 10;
+    }
+    return y === x;
+    
+};
+module.exports = isPalindrome;
---
+++ js/9. Palindrome Number/PalindromeNumber.test.js
@@ -0,0 +1,16 @@
+﻿const isPalindrome = require('./PalindromeNumber');
+
+test('palindrome number 1', () => {
+  const num = 121;
+  expect(isPalindrome(num)).toBe(true);
+});
+
+test('palindrome number 2', () => {
+  const num = -121;
+  expect(isPalindrome(num)).toBe(false);
+});
+
+test('palindrome number 3', () => {
+  const num = 10;
+  expect(isPalindrome(num)).toBe(false);
+});
