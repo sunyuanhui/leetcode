f971cde0df752b02fe59cc02e12abcad9eab2bd9
孙源辉 <sunyuanhui@wisonic.cn>
2020-01-31 19:43:54 +0800

    完成 8. 字符串转换整数 (atoi)

---
+++ js/8. String to Integer/StringtoInteger.js
@@ -0,0 +1,35 @@
+﻿/**
+ * @param {string} str
+ * @return {number}
+ */
+var myAtoi = function(str) {
+    const INT_MAX = 2147483647;
+    const INT_MIN = -2147483648;
+    
+    str = str.trim();
+    if (str.length === 0) {
+        return 0;
+    }
+
+    const sign = str[0] === '-' ? -1 : 1;
+    if (str[0] === '+' || str[0] === '-') {
+        str = str.substring(1);
+    }
+
+    let result = 0;
+    for (let i = 0; i < str.length; i++) {
+        const c = str[i];
+        if (c < '0' || c > '9') {
+            return result * sign;
+        }
+        result = result * 10 + Number.parseInt(c);
+        if (sign === 1 && result > INT_MAX) {
+            return INT_MAX;
+        }
+        if (sign === -1 && result * sign < INT_MIN) {
+            return INT_MIN;
+        }
+    }
+    return result * sign;
+};
+module.exports = myAtoi;
---
+++ js/8. String to Integer/StringtoInteger.test.js
@@ -0,0 +1,26 @@
+﻿const myAtoi = require('./StringtoInteger');
+
+test('string to integer 1', () => {
+  const str = '42';
+  expect(myAtoi(str)).toBe(42);
+});
+
+test('string to integer 2', () => {
+  const str = '-42';
+  expect(myAtoi(str)).toBe(-42);
+});
+
+test('string to integer 3', () => {
+  const str = '4193 with words';
+  expect(myAtoi(str)).toBe(4193);
+});
+
+test('string to integer 4', () => {
+  const str = 'words and 987';
+  expect(myAtoi(str)).toBe(0);
+});
+
+test('string to integer 5', () => {
+  const str = '-91283472332';
+  expect(myAtoi(str)).toBe(-2147483648);
+});
