5f754827cef41a5348a390aa95c6b6d2e4bf2e07
孙源辉 <sunyuanhui@wisonic.cn>
2020-02-01 22:51:02 +0800

    const s = "aasdfasdfasdfasdfas"; const p = "aasdf.*asdf.*asdf.*asdf.*s"; expect(isMatch(s, p)).toBe(true); 走到这个测试用例挂掉了，思路不对

---
+++ js/10. Regular Expression Matching/RegularExpressionMatching.js
@@ -0,0 +1,69 @@
+﻿/**
+ * @param {string} s
+ * @param {string} p
+ * @return {boolean}
+ * '.' Matches any single character.
+ * '*' Matches zero or more of the preceding element.
+ */
+var isMatch = function(s, p) {
+    let sfi = 0;
+    let pfi = 0;
+    let asteriskMatched = 0;
+    while (sfi < s.length && pfi < p.length) {
+        if ((pfi + 1 < p.length) && p[pfi + 1] === '*') {
+            while (sfi < s.length) {
+                if (s[sfi] === p[pfi] || p[pfi] === '.') {
+                    asteriskMatched++;
+                    sfi++;
+                } else {
+                    break;
+                }
+            }
+            pfi = pfi + 2;
+        } else {
+            if (s[sfi] === p[pfi] || p[pfi] === '.') {
+                sfi++;
+                pfi++;
+            } else {
+                return false;
+            }
+        }
+    }
+
+    if (pfi < p.length && asteriskMatched !== 0) {
+        let sbi = s.length - 1;
+        let pbi = p.length - 1;
+        while (pfi <= pbi && asteriskMatched >= s.length - sbi) {
+            if (p[pbi] === '*') {
+                if (pfi === pbi) {
+                    return false;
+                }
+
+                while (asteriskMatched >= s.length - sbi) {
+                    if (s[sbi] === p[pbi - 1] || p[pbi - 1] === '.') {
+                        sbi--;
+                    } else {
+                        break;
+                    }
+                }
+                pbi = pbi - 2;
+            } else if (s[sbi] === p[pbi] || p[pbi] === '.') {
+                sbi--;
+                pbi--;
+            } else {
+                return false;
+            }
+        }
+        return pfi - 1 === pbi;
+    }
+
+    while (pfi < p.length - 1) {
+        if (p[pfi] !== '*' && p[pfi + 1] === '*') {
+            pfi = pfi + 2;
+        } else {
+            break;
+        }
+    }
+    return (sfi === s.length) && (pfi === p.length);
+};
+module.exports = isMatch;
---
+++ js/10. Regular Expression Matching/RegularExpressionMatching.test.js
@@ -0,0 +1,139 @@
+﻿const isMatch = require('./RegularExpressionMatching');
+
+test('regular expression matching 1', () => {
+  const s = "aa";
+  const p = "a";
+  expect(isMatch(s, p)).toBe(false);
+});
+
+test('regular expression matching 2', () => {
+  const s = "aa";
+  const p = "a*";
+  expect(isMatch(s, p)).toBe(true);
+});
+
+test('regular expression matching 3', () => {
+  const s = "aa";
+  const p = ".*";
+  expect(isMatch(s, p)).toBe(true);
+});
+
+test('regular expression matching 4', () => {
+  const s = "ab";
+  const p = ".*";
+  expect(isMatch(s, p)).toBe(true);
+});
+
+test('regular expression matching 5', () => {
+  const s = 'mississippi';
+  const p = 'mis*is*p*.';
+  expect(isMatch(s, p)).toBe(false);
+});
+
+test('regular expression matching 6', () => {
+  const s = "aab";
+  const p = "c*a*b";
+  expect(isMatch(s, p)).toBe(true);
+});
+
+test('regular expression matching 7', () => {
+  const s = "aab";
+  const p = "a*c*b";
+  expect(isMatch(s, p)).toBe(true);
+});
+
+test('regular expression matching 8', () => {
+  const s = "aaa";
+  const p = "ab*a*c*a";
+  expect(isMatch(s, p)).toBe(true);
+});
+
+test('regular expression matching 9', () => {
+  const s = "aaa";
+  const p = "a*a";
+  expect(isMatch(s, p)).toBe(true);
+});
+
+test('regular expression matching 10', () => {
+  const s = "aaa";
+  const p = "a*aaa";
+  expect(isMatch(s, p)).toBe(true);
+});
+
+test('regular expression matching 11', () => {
+  const s = "aaa";
+  const p = "a*aaaa";
+  expect(isMatch(s, p)).toBe(false);
+});
+
+test('regular expression matching 12', () => {
+  const s = "aaa";
+  const p = "a*.";
+  expect(isMatch(s, p)).toBe(true);
+});
+
+test('regular expression matching 13', () => {
+  const s = "aaa";
+  const p = "a*..";
+  expect(isMatch(s, p)).toBe(true);
+});
+
+test('regular expression matching 14', () => {
+  const s = "aaa";
+  const p = "a*...";
+  expect(isMatch(s, p)).toBe(true);
+});
+
+test('regular expression matching 15', () => {
+  const s = "aaa";
+  const p = "a*....";
+  expect(isMatch(s, p)).toBe(false);
+});
+
+test('regular expression matching 16', () => {
+  const s = "aaab";
+  const p = "a*..";
+  expect(isMatch(s, p)).toBe(true);
+});
+
+test('regular expression matching 17', () => {
+  const s = "aaa";
+  const p = "...a*";
+  expect(isMatch(s, p)).toBe(true);
+});
+
+test('regular expression matching 18', () => {
+  const s = "aaa";
+  const p = "a*..b*";
+  expect(isMatch(s, p)).toBe(true);
+});
+
+test('regular expression matching 19', () => {
+  const s = "aaa";
+  const p = "....a*";
+  expect(isMatch(s, p)).toBe(false);
+});
+
+test('regular expression matching 20', () => {
+  const s = "baaa";
+  const p = "..a*";
+  expect(isMatch(s, p)).toBe(true);
+});
+
+test('regular expression matching 21', () => {
+  const s = "baaa";
+  const p = ".....a*";
+  expect(isMatch(s, p)).toBe(false);
+});
+
+test('regular expression matching 22', () => {
+  const s = "";
+  const p = ".*";
+  expect(isMatch(s, p)).toBe(true);
+});
+
+test('regular expression matching 23', () => {
+  const s = "aasdfasdfasdfasdfas";
+  const p = "aasdf.*asdf.*asdf.*asdf.*s";
+  expect(isMatch(s, p)).toBe(true);
+});
