1a6a16874383dc06e57d6b6c91cb655c478df3c5
孙源辉 <sunyuanhui@wisonic.cn>
2020-02-02 00:13:01 +0800

    参考了题解，完成了 10. 正则表达式匹配

--- js/10. Regular Expression Matching/RegularExpressionMatching.js
+++ js/10. Regular Expression Matching/RegularExpressionMatching.js
@@ -6,64 +6,18 @@
  * '*' Matches zero or more of the preceding element.
  */
 var isMatch = function(s, p) {
-    let sfi = 0;
-    let pfi = 0;
-    let asteriskMatched = 0;
-    while (sfi < s.length && pfi < p.length) {
-        if ((pfi + 1 < p.length) && p[pfi + 1] === '*') {
-            while (sfi < s.length) {
-                if (s[sfi] === p[pfi] || p[pfi] === '.') {
-                    asteriskMatched++;
-                    sfi++;
-                } else {
-                    break;
-                }
-            }
-            pfi = pfi + 2;
-        } else {
-            if (s[sfi] === p[pfi] || p[pfi] === '.') {
-                sfi++;
-                pfi++;
-            } else {
-                return false;
-            }
-        }
+    if (p.length === 0) {
+        return s.length === 0;
     }
 
-    if (pfi < p.length && asteriskMatched !== 0) {
-        let sbi = s.length - 1;
-        let pbi = p.length - 1;
-        while (pfi <= pbi && asteriskMatched >= s.length - sbi) {
-            if (p[pbi] === '*') {
-                if (pfi === pbi) {
-                    return false;
-                }
+    const firstMatched = s.length !== 0 && (s[0] === p[0] || p[0] === '.');
 
-                while (asteriskMatched >= s.length - sbi) {
-                    if (s[sbi] === p[pbi - 1] || p[pbi - 1] === '.') {
-                        sbi--;
-                    } else {
-                        break;
-                    }
-                }
-                pbi = pbi - 2;
-            } else if (s[sbi] === p[pbi] || p[pbi] === '.') {
-                sbi--;
-                pbi--;
-            } else {
-                return false;
-            }
-        }
-        return pfi - 1 === pbi;
+    if (p.length >= 2 && p[1] === '*') {
+        return isMatch(s, p.substring(2)) 
+            || (firstMatched && isMatch(s.substring(1), p));
+    } else {
+        return firstMatched && s.length !== 0 
+            && isMatch(s.substring(1), p.substring(1));
     }
-
-    while (pfi < p.length - 1) {
-        if (p[pfi] !== '*' && p[pfi + 1] === '*') {
-            pfi = pfi + 2;
-        } else {
-            break;
-        }
-    }
-    return (sfi === s.length) && (pfi === p.length);
 };
 module.exports = isMatch;
