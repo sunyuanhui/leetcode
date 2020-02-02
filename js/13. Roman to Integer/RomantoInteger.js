b34f4bad70dfe9f30e30cbdcb494e78271ebc1e6
孙源辉 <sunyuanhui@wisonic.cn>
2020-02-02 19:01:09 +0800

    完成 13. 罗马数字转整数

---
+++ js/13. Roman to Integer/RomantoInteger.js
@@ -0,0 +1,24 @@
+﻿/**
+ * @param {string} s
+ * @return {number}
+ */
+var romanToInt = function(s) {
+    const romans = {
+        'I': 1, 'IV': 4, 'V': 5, 'IX': 9, 'X': 10, 'XL': 40, 'L': 50,
+        'XC': 90, 'C': 100, 'CD': 400, 'D': 500, 'CM': 900, 'M': 1000
+    };
+
+    let num = 0;
+    while (s.length > 1) {
+        let c = s.substring(0, 2);
+        if (!romans.hasOwnProperty(c)) {
+            c = s[0];
+            s = s.substring(1);
+        } else {
+            s = s.substring(2);
+        }
+        num = num + romans[c];
+    }
+    return num + (s.length !== 0 ? romans[s] : 0);
+};
+module.exports = romanToInt;
---
+++ js/13. Roman to Integer/RomantoInteger.test.js
@@ -0,0 +1,29 @@
+﻿const romanToInt = require('./RomantoInteger');
+
+test('roman to integer 1', () => {
+  const str = "III";
+  expect(romanToInt(str)).toBe(3);
+});
+
+test('roman to integer 2', () => {
+  const str = "IV";
+  expect(romanToInt(str)).toBe(4);
+});
+
+test('roman to integer 3', () => {
+  const str = "IX";
+  expect(romanToInt(str)).toBe(9);
+});
+
+test('roman to integer 4', () => {
+  const str = "LVIII";
+  // Explanation: L = 50, V= 5, III = 3.
+  expect(romanToInt(str)).toBe(58);
+});
+
+test('roman to integer 5', () => {
+  const str = "MCMXCIV";
+  // Explanation: M = 1000, CM = 900, XC = 90 and IV = 4.
+  expect(romanToInt(str)).toBe(1994);
+});
+
