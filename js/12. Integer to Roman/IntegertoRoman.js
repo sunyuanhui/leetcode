765802496ee9ef794e87fb1b3d6f1e981dabb3c9
孙源辉 <sunyuanhui@wisonic.cn>
2020-02-02 18:26:49 +0800

    完成 12. 整数转罗马数字

---
+++ js/12. Integer to Roman/IntegertoRoman.js
@@ -0,0 +1,28 @@
+﻿/**
+ * @param {number} num
+ * @return {string}
+ * Symbol       Value
+ * I             1
+ * V             5
+ * X             10
+ * L             50
+ * C             100
+ * D             500
+ * M             1000
+ */
+var intToRoman = function(num) {
+    const romans = [
+        ['', 'I', 'II', 'III', 'IV', 'V', 'VI', 'VII', 'VIII', 'IX'],
+        ['', 'X', 'XX', 'XXX', 'XL', 'L', 'LX', 'LXX', 'LXXX', 'XC'],
+        ['', 'C', 'CC', 'CCC', 'CD', 'D', 'DC', 'DCC', 'DCCC', 'CM'],
+        ['', 'M', 'MM', 'MMM']
+    ];
+    let roman = '';
+    for (let i = 0; num !== 0; i++) {
+        const n = num % 10;
+        num = (num - num % 10) / 10;
+        roman = romans[i][n] + roman;
+    }
+    return roman;
+};
+module.exports = intToRoman;
---
+++ js/12. Integer to Roman/IntegertoRoman.test.js
@@ -0,0 +1,29 @@
+﻿const intToRoman = require('./IntegertoRoman');
+
+test('integer to roman 1', () => {
+  const num = 3;
+  expect(intToRoman(num)).toBe("III");
+});
+
+test('integer to roman 2', () => {
+  const num = 4;
+  expect(intToRoman(num)).toBe("IV");
+});
+
+test('integer to roman 3', () => {
+  const num = 9;
+  expect(intToRoman(num)).toBe("IX");
+});
+
+test('integer to roman 4', () => {
+  const num = 58;
+  // Explanation: L = 50, V = 5, III = 3.
+  expect(intToRoman(num)).toBe("LVIII");
+});
+
+test('integer to roman 5', () => {
+  const num = 1994;
+  // Explanation: M = 1000, CM = 900, XC = 90 and IV = 4.
+  expect(intToRoman(num)).toBe("MCMXCIV");
+});
+
