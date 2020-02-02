f52789274209faf586727f6544d5ee2dedf1ee72
孙源辉 <sunyuanhui@wisonic.cn>
2020-02-02 14:44:19 +0800

    完成 11. 盛最多水的容器，但是性能一般

---
+++ js/11. Container With Most Water/ContainerWithMostWater.js
@@ -0,0 +1,14 @@
+﻿/**
+ * @param {number[]} height
+ * @return {number}
+ */
+var maxArea = function(height) {
+    let max = 0;
+    for (let i = 0; i < height.length; i++) {
+        for (let j = i + 1; j < height.length; j++) {
+            max = Math.max(max, (j - i) * Math.min(height[i], height[j]));
+        }
+    }
+    return max;
+};
+module.exports = maxArea;
---
+++ js/11. Container With Most Water/ContainerWithMostWater.test.js
@@ -0,0 +1,7 @@
+﻿const maxArea = require('./ContainerWithMostWater');
+
+test('container with most water 1', () => {
+  const num = [1,8,6,2,5,4,8,3,7];
+  expect(maxArea(num)).toBe(49);
+});
+
