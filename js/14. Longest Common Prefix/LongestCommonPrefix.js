213912c28aa4974f443906d6089079a9c2a73504
孙源辉 <sunyuanhui@wisonic.cn>
2020-02-03 23:38:34 +0800

    完成 14. 最长公共前缀，但性能不好

---
+++ js/14. Longest Common Prefix/LongestCommonPrefix.js
@@ -0,0 +1,34 @@
+﻿/**
+ * @param {string[]} strs
+ * @return {string}
+ */
+var longestCommonPrefix = function(strs) {
+    if (strs.length === 0) {
+        return '';
+    }
+
+    let prefix = '';
+    let i = 0;
+    while (1) {
+        let c = '';
+        for (let j = 0; j < strs.length; j++) {
+            if (i === strs[j].length) {
+                return prefix;
+            }
+
+            if (j === 0) {
+                c = strs[j][i];
+            } else {
+                if (c !== strs[j][i]) {
+                    return prefix;
+                }
+            }
+
+            if (j === strs.length - 1) {
+                i++;
+                prefix = prefix + c;
+            }
+        }
+    }
+};
+module.exports = longestCommonPrefix;
---
+++ js/14. Longest Common Prefix/LongestCommonPrefix.test.js
@@ -0,0 +1,11 @@
+﻿const longestCommonPrefix = require('./LongestCommonPrefix');
+
+test('longest common prefix 1', () => {
+  const strs = ["flower","flow","flight"];
+  expect(longestCommonPrefix(strs)).toBe('fl');
+});
+
+test('longest common prefix 2', () => {
+  const strs = ["dog","racecar","car"];
+  expect(longestCommonPrefix(strs)).toBe('');
+});
