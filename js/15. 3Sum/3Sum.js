1b749d0d3b306223268a7a59ca73a8171e94fdf7
孙源辉 <sunyuanhui@wisonic.cn>
2020-02-04 22:42:26 +0800

    还是不行，哈哈

--- js/15. 3Sum/3Sum.js
+++ js/15. 3Sum/3Sum.js
@@ -5,35 +5,50 @@
 var threeSum = function(nums) {
     let m = new Map();
     for (let i = 0; i < nums.length; i++) {
-        let v = nums[i]
-        if (m.has(v)) {
-            m.get(v).add(i);
+        let k = nums[i]
+        if (m.has(k)) {
+            m.get(k).add(i);
         } else {
-            m.set(v, new Set([i]));
+            m.set(k, new Set([i]));
         }
     }
 
+    nums = Array.from(m.keys());
     let result = [];
     let resultStrs = [];
     for (let i = 0; i < nums.length - 2; i++) {
         for (let j = i + 1; j < nums.length - 1; j ++) {
-            const c = 0 - nums[i] - nums[j];
-            if (!m.has(c)) {
+            let a = nums[i];
+            let b = nums[j];
+            const c = 0 - a - b;
+            if (!m.has(c) || a === c || b === c) {
                 continue;
             }
 
-            let s = new Set(m.get(c));
-            s.delete(i);
-            s.delete(j);
-            if (s.size === 0) {
-                continue;
-            }
-
-            let r = [nums[i], nums[j], c].sort((a, b) => a - b);
+            let r = [a, b, c].sort((x, y) => x - y);
             if (!resultStrs.includes(r.toString())) {
                 result.push(r);
                 resultStrs.push(r.toString());
             }
+        }
+    }
+
+    for (let i = 0; i < nums.length; i++) {
+        let a = nums[i];
+        let v = m.get(a);
+        if (v.size < 2 || (a === 0 && v.size ===2)) {
+            continue;
+        }
+
+        const c = 0 - a * 2;
+        if (!m.has(c)) {
+            continue;
+        }
+
+        let r = [a, a, c].sort((a, b) => a - b);
+        if (!resultStrs.includes(r.toString())) {
+            result.push(r);
+            resultStrs.push(r.toString());
         }
     }
     return result;
--- js/15. 3Sum/3Sum.test.js
+++ js/15. 3Sum/3Sum.test.js
@@ -5,3 +5,15 @@ test('3sum 1', () => {
   const solution = [ [-1, 0, 1], [-1, -1, 2] ];
   expect(threeSum(nums)).toEqual(solution);
 });
+
+test('3sum 2', () => {
+  const nums = [0, 0];
+  const solution = [];
+  expect(threeSum(nums)).toEqual(solution);
+});
+
+test('3sum 3', () => {
+  const nums = [-4,-2,-2,-2,0,1,2,2,2,3,3,4,4,6,6];
+  const solution = [[-4,-2,6],[-4,0,4],[-4,1,3],[-4,2,2],[-2,-2,4],[-2,0,2]].sort();
+  expect(threeSum(nums).sort()).toEqual(solution);
+});
