0dea1fa2962dc2a1f5166d9cc2ceb9615883a9fb
孙源辉 <sunyuanhui@wisonic.cn>
2020-02-04 21:28:42 +0800

    完成 15. 三数之和，不过运行超时了，需要优化

---
+++ js/15. 3Sum/3Sum.js
@@ -0,0 +1,63 @@
+﻿/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+var threeSum = function(nums) {
+    let m = new Map();
+    for (let i = 0; i < nums.length; i++) {
+        let v = nums[i]
+        if (m.has(v)) {
+            m.get(v).add(i);
+        } else {
+            m.set(v, new Set([i]));
+        }
+    }
+
+    let result = [];
+    let resultStrs = [];
+    for (let i = 0; i < nums.length - 2; i++) {
+        for (let j = i + 1; j < nums.length - 1; j ++) {
+            const c = 0 - nums[i] - nums[j];
+            if (!m.has(c)) {
+                continue;
+            }
+
+            let s = new Set(m.get(c));
+            s.delete(i);
+            s.delete(j);
+            if (s.size === 0) {
+                continue;
+            }
+
+            let min = c;
+            let mid;
+            let max;
+            if (min < nums[i]) {
+                mid = nums[i];
+            } else {
+                mid = min;
+                min = nums[i];
+            }
+            if (mid < nums[j]) {
+                max = nums[j];
+            } else {
+                max = mid;
+                if (min < nums[j]) {
+                    mid = nums[j];
+                } else {
+                    mid = min;
+                    min = nums[j];
+                }
+            }
+
+            let r = [min, mid, max];
+            let str = min + '' + mid + '' + max;
+            if (!resultStrs.includes(str)) {
+                result.push(r);
+                resultStrs.push(str);
+            }
+        }
+    }
+    return result;
+};
+module.exports = threeSum;
---
+++ js/15. 3Sum/3Sum.test.js
@@ -0,0 +1,7 @@
+﻿const threeSum = require('./3Sum');
+
+test('3sum 1', () => {
+  const nums = [-1, 0, 1, 2, -1, -4];
+  const solution = [ [-1, 0, 1], [-1, -1, 2] ];
+  expect(threeSum(nums)).toEqual(solution);
+});
