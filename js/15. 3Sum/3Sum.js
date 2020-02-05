0d159ea51132188d37e325a64d7fb28811cf4be6
孙源辉 <sunyuanhui@wisonic.cn>
2020-02-05 12:10:57 +0800

    终于通过了

--- js/15. 3Sum/3Sum.js
+++ js/15. 3Sum/3Sum.js
@@ -3,26 +3,27 @@
  * @return {number[][]}
  */
 var threeSum = function(nums) {
-    let m = new Map();
-    for (let i = 0; i < nums.length; i++) {
-        let count = m.get(nums[i]);
-        m.set(nums[i], count === undefined ? 1 : count + 1);
-    }
-    if (m.size < 2) {
-        return m.get(0) > 2 ? [[0, 0, 0]] : [];
-    }
-
     nums = nums.sort((x, y) => x - y);
 
     let result = [];
     let resultStrs = [];
     for (let i = 0; i < nums.length - 2; i++) {
         let a = nums[i];
+        if (a > 0) {
+            break;
+        }
+        if (i > 0 && a === nums[i - 1]) {
+            continue;
+        }
+
         let j = i + 1;
         let k = nums.length - 1;
         while (j < k) {
             let b = nums[j];
             let c = nums[k];
+            if (a < 0 && b < 0 && c < 0) {
+                break;
+            }
             if (a + b + c === 0) {
                 let r = [a, b, c].sort((x, y) => x - y);
                 if (!resultStrs.includes(r.toString())) {
@@ -37,9 +38,6 @@ var threeSum = function(nums) {
             }
             if (a + b + c < 0) {
                 j++;
-            }
-            if ((a > 0 && b > 0 && c > 0) || (a < 0 && b < 0 && c < 0)) {
-                break;
             }
         }
     }
