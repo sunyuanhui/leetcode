b298919061070d78003bed108c13288d55f4942e
孙源辉 <sunyuanhui@wisonic.cn>
2020-02-05 11:44:10 +0800

    测试用例全部通过没有超时的测试用例，不过总体超出了时间限制

--- js/15. 3Sum/3Sum.js
+++ js/15. 3Sum/3Sum.js
@@ -3,22 +3,43 @@
  * @return {number[][]}
  */
 var threeSum = function(nums) {
+    let m = new Map();
+    for (let i = 0; i < nums.length; i++) {
+        let count = m.get(nums[i]);
+        m.set(nums[i], count === undefined ? 1 : count + 1);
+    }
+    if (m.size < 2) {
+        return m.get(0) > 2 ? [[0, 0, 0]] : [];
+    }
+
+    nums = nums.sort((x, y) => x - y);
+
     let result = [];
     let resultStrs = [];
-    for (let i = 0; i < nums.length - 1; i++) {
-        for (let j = i + 1; j < nums.length; j ++) {
-            let a = nums[i];
+    for (let i = 0; i < nums.length - 2; i++) {
+        let a = nums[i];
+        let j = i + 1;
+        let k = nums.length - 1;
+        while (j < k) {
             let b = nums[j];
-            const c = 0 - a - b;
-            const index = nums.indexOf(c);
-            if (index === -1 || index === i || index === j) {
-                continue;
+            let c = nums[k];
+            if (a + b + c === 0) {
+                let r = [a, b, c].sort((x, y) => x - y);
+                if (!resultStrs.includes(r.toString())) {
+                    result.push(r);
+                    resultStrs.push(r.toString());
+                }
+                j++;
+                k--;
             }
-
-            let r = [a, b, c].sort((x, y) => x - y);
-            if (!resultStrs.includes(r.toString())) {
-                result.push(r);
-                resultStrs.push(r.toString());
+            if (a + b + c > 0) {
+                k--;
+            }
+            if (a + b + c < 0) {
+                j++;
+            }
+            if ((a > 0 && b > 0 && c > 0) || (a < 0 && b < 0 && c < 0)) {
+                break;
             }
         }
     }
