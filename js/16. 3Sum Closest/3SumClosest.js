7721b6a9e019b5908641dc714983e4ab1ea690e1
孙源辉 <sunyuanhui@wisonic.cn>
2020-02-08 00:09:31 +0800

    先用暴力算法完成 16. 最接近的三数之和

---
+++ js/16. 3Sum Closest/3SumClosest.js
@@ -0,0 +1,22 @@
+﻿/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var threeSumClosest = function(nums, target) {
+    let result = undefined;
+    for (let i = 0; i < nums.length - 2; i++) {
+        for (let j = i + 1; j < nums.length - 1; j++) {
+            for (let k = j + 1; k < nums.length; k++) {
+                let r = nums[i] + nums[j] + nums[k];
+                let n = Math.abs(target - r);
+                let o = Math.abs(target - result);
+                if (result === undefined || n < o) {
+                    result = r;
+                }
+            }
+        }
+    }
+    return result;
+};
+module.exports = threeSumClosest;
---
+++ js/16. 3Sum Closest/3SumClosest.test.js
@@ -0,0 +1,33 @@
+﻿const threeSumClosest = require('./3SumClosest');
+
+test('3sum closest 1', () => {
+  const nums = [-1, 2, 1, -4];
+  const target = 1;
+  expect(threeSumClosest(nums, target)).toBe(2);
+});
+
+test('3sum closest 2', () => {
+  const nums = [1, 1, -1, -1, 3];
+  const target = 3;
+  expect(threeSumClosest(nums, target)).toBe(3);
+});
+
+test('3sum closest 3', () => {
+  const nums = [1, 2, 4, 8, 16, 32, 64, 128];
+  const target = 82;
+  expect(threeSumClosest(nums, target)).toBe(82);
+});
+
+test('3sum closest 4', () => {
+  const nums = [0, 1, 2];
+  const target = 0;
+  expect(threeSumClosest(nums, target)).toBe(3);
+});
+
+test('3sum closest 5', () => {
+  const nums = [0, 2, 1, -3];
+  const target = 1;
+  expect(threeSumClosest(nums, target)).toBe(0);
+});
+
+
