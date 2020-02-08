ea00826f8b63888d1c531482697a830d7749d14b
孙源辉 <sunyuanhui@wisonic.cn>
2020-02-08 17:07:34 +0800

    完成 18. 四数之和

---
+++ js/18. 4Sum/4Sum.js
@@ -0,0 +1,51 @@
+﻿/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[][]}
+ */
+var fourSum = function(nums, target) {
+    if (nums.length < 4) {
+        return [];
+    }
+    nums.sort((a, b) => a - b);
+    
+    let result = [];
+    for (let i = 0; i < nums.length - 3; i++) {
+        for (let j = i + 1; j < nums.length - 2; j++) {
+            let k = j + 1;
+            let m = nums.length - 1;
+            while (k < m) {
+                let sum = nums[i] + nums[j] + nums[k] + nums[m];
+                if (sum === target) {
+                    result.push([nums[i], nums[j], nums[k], nums[m]])
+                    while (k < m && nums[k] === nums[k + 1]) {
+                        k++;
+                    }
+                    k++;
+                    while (k < m && nums[m - 1] === nums[m]) {
+                        m--;
+                    }
+                    m--;
+                } else if (sum > target) {
+                    while (k < m && nums[m - 1] === nums[m]) {
+                        m--;
+                    }
+                    m--;
+                } else {
+                    while (k < m && nums[k] === nums[k + 1]) {
+                        k++;
+                    }
+                    k++;
+                }
+            }
+            while (j < nums.length - 2 && nums[j] === nums[j + 1]) {
+                j++;
+            }
+        }
+        while (i < nums.length - 3 && nums[i] === nums[i + 1]) {
+            i++;
+        }
+    }
+    return result;
+};
+module.exports = fourSum;
---
+++ js/18. 4Sum/4Sum.test.js
@@ -0,0 +1,22 @@
+﻿const fourSum = require('./4Sum');
+
+test('4sum 1', () => {
+  const nums = [1, 0, -1, 0, -2, 2];
+  const target = 0;
+  const solution = [ [-1, 0, 0, 1], [-2, -1, 1, 2], [-2, 0, 0, 2] ].sort();
+  expect(fourSum(nums, target).sort()).toEqual(solution);
+});
+
+test('4sum 2', () => {
+  const nums = [-3,-2,-1,0,0,1,2,3];
+  const target = 0;
+  const solution = [[-3,-2,2,3],[-3,-1,1,3],[-3,0,0,3],[-3,0,1,2],[-2,-1,0,3],[-2,-1,1,2],[-2,0,0,2],[-1,0,0,1]].sort();
+  expect(fourSum(nums, target).sort()).toEqual(solution);
+});
+
+test('4sum 3', () => {
+  const nums = [-1,-5,-5,-3,2,5,0,4];
+  const target = -7;
+  const solution = [[-5,-5,-1,4],[-5,-3,-1,2]].sort();
+  expect(fourSum(nums, target).sort()).toEqual(solution);
+});
