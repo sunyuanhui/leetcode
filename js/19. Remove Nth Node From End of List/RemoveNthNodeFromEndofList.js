afcecbc9d9d9790d768300d17f77f0b0b4bb6f56
孙源辉 <sunyuanhui@wisonic.cn>
2020-02-08 18:25:05 +0800

    完成 19. 删除链表的倒数第N个节点

---
+++ js/19. Remove Nth Node From End of List/RemoveNthNodeFromEndofList.js
@@ -0,0 +1,35 @@
+﻿/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @param {number} n
+ * @return {ListNode}
+ */
+var removeNthFromEnd = function(head, n) {
+    let array = [];
+    let tmp = head;
+    while (tmp) {
+        array.push(tmp);
+        tmp = tmp.next;
+    }
+
+    if (n > array.length || n <= 0) {
+        return head;
+    } else if (n === array.length) {
+        return head.next;
+    }
+
+    let pre = array.length - n - 1;
+    array[pre].next = array[pre].next === null ? null : array[pre].next.next;
+    return head;
+};
+function ListNode(val) {
+    this.val = val;
+    this.next = null;
+}
+module.exports = { removeNthFromEnd, ListNode };
---
+++ js/19. Remove Nth Node From End of List/RemoveNthNodeFromEndofList.test.js
@@ -0,0 +1,32 @@
+﻿const { removeNthFromEnd, ListNode } = require('./RemoveNthNodeFromEndofList');
+
+var ArrayToList = function(array) {
+  const l = new ListNode(array[0]);
+  let t = l;
+  for (let i = 1; i < array.length; i++) {
+    t.next = new ListNode(array[i]);
+    t = t.next;
+  }
+  return l;
+}
+var ListToArray = function(list) {
+  let a = []
+  while (list !== null) {
+    a.push(list.val);
+    list = list.next;
+  }
+  return a;
+}
+
+test('remove nth node from end of list 1', () => {
+  const l = ArrayToList([1, 2, 3, 4, 5]);
+  const n = 2;
+  expect(ListToArray(removeNthFromEnd(l, n))).toEqual([1, 2, 3, 5]);
+});
+
+test('remove nth node from end of list 2', () => {
+  const l = ArrayToList([1]);
+  const n = 1;
+  expect(ListToArray(removeNthFromEnd(l, n))).toEqual([]);
+});
+
