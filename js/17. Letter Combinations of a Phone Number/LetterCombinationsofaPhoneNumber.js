f13d2a5bf5b16e2721bfdcf7f258797132a24060
孙源辉 <sunyuanhui@wisonic.cn>
2020-02-08 15:26:47 +0800

    完成 17. 电话号码的字母组合，应该还可以再优化一下

---
+++ js/17. Letter Combinations of a Phone Number/LetterCombinationsofaPhoneNumber.js
@@ -0,0 +1,36 @@
+﻿/**
+ * @param {string} digits
+ * @return {string[]}
+ */
+var letterCombinations = function(digits) {
+    const lettersMap = {
+        '2': ['a', 'b', 'c'],
+        '3': ['d', 'e', 'f'],
+        '4': ['g', 'h', 'i'],
+        '5': ['j', 'k', 'l'],
+        '6': ['m', 'n', 'o'],
+        '7': ['p', 'q', 'r', 's'],
+        '8': ['t', 'u', 'v'],
+        '9': ['w', 'x', 'y', 'z']
+    };
+    
+    if (digits === '') {
+        return [];
+    }
+    return find(lettersMap, lettersMap[digits[0]], digits.substring(1));
+};
+var find = function(lettersMap, letters, left) {
+    if (left === '') {
+        return letters;
+    }
+
+    let result = [];
+    for (let i = 0; i < letters.length; i++) {
+        let nextLetters = find(lettersMap, lettersMap[left[0]], left.substring(1));
+        for (let j = 0; j < nextLetters.length; j++) {
+            result.push(letters[i] + nextLetters[j]);
+        }
+    }
+    return result;
+};
+module.exports = letterCombinations;
---
+++ js/17. Letter Combinations of a Phone Number/LetterCombinationsofaPhoneNumber.test.js
@@ -0,0 +1,7 @@
+﻿const letterCombinations = require('./LetterCombinationsofaPhoneNumber');
+
+test('letter combinations of a phone number 1', () => {
+  const digits = "23";
+  expect(letterCombinations(digits).sort()).toEqual(["ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf"].sort());
+});
+
