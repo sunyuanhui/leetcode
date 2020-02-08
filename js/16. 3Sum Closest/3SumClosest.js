e423609b7f2371cb91b2cd60c065ec7e37ca1a2f
孙源辉 <sunyuanhui@wisonic.cn>
2020-02-08 13:47:35 +0800

    性能提到到156ms

--- js/16. 3Sum Closest/3SumClosest.js
+++ js/16. 3Sum Closest/3SumClosest.js
@@ -4,19 +4,42 @@
  * @return {number}
  */
 var threeSumClosest = function(nums, target) {
+    nums.sort((a, b) => a - b);
+
     let result = undefined;
     for (let i = 0; i < nums.length - 2; i++) {
+        let jResult = undefined
         for (let j = i + 1; j < nums.length - 1; j++) {
+            let kResult = undefined;
             for (let k = j + 1; k < nums.length; k++) {
                 let r = nums[i] + nums[j] + nums[k];
-                let n = Math.abs(target - r);
-                let o = Math.abs(target - result);
-                if (result === undefined || n < o) {
-                    result = r;
+                if (r === target) {
+                    return r;
+                }
+
+                kResult = calc(target, r, kResult);
+                if (kResult !== r) {
+                    break;
                 }
             }
+
+            jResult = calc(target, kResult, jResult);
+            if (jResult !== kResult) {
+                break;
+            }
         }
+
+        result = calc(target, jResult, result);
     }
     return result;
+};
+var calc = function(target, newResult, oldResult) {
+    let n = Math.abs(target - newResult);
+    let o = Math.abs(target - oldResult);
+    if (oldResult === undefined || n < o) {
+        return newResult;
+    } else {
+        return oldResult;
+    }
 };
 module.exports = threeSumClosest;
