3df2bce84b6fe6416d9a41df2648f2a9cf34a2c7
孙源辉 <sunyuanhui@wisonic.cn>
2020-02-08 15:43:56 +0800

    增加了缓存提升性能到50ms+

--- js/17. Letter Combinations of a Phone Number/LetterCombinationsofaPhoneNumber.js
+++ js/17. Letter Combinations of a Phone Number/LetterCombinationsofaPhoneNumber.js
@@ -17,16 +17,23 @@ var letterCombinations = function(digits) {
     if (digits === '') {
         return [];
     }
-    return find(lettersMap, lettersMap[digits[0]], digits.substring(1));
+    let mem = {};
+    return find(lettersMap, lettersMap[digits[0]], digits.substring(1), mem);
 };
-var find = function(lettersMap, letters, left) {
+var find = function(lettersMap, letters, left, mem) {
     if (left === '') {
         return letters;
     }
 
     let result = [];
     for (let i = 0; i < letters.length; i++) {
-        let nextLetters = find(lettersMap, lettersMap[left[0]], left.substring(1));
+        let nextLetters;
+        if (mem[left] !== undefined) {
+            nextLetters = mem[left];
+        } else {
+            nextLetters = find(lettersMap, lettersMap[left[0]], left.substring(1), mem);
+            mem[left] = nextLetters;
+        }
         for (let j = 0; j < nextLetters.length; j++) {
             result.push(letters[i] + nextLetters[j]);
         }
