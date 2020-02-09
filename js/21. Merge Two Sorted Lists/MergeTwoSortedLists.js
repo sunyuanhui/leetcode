d20934a915a8d3c51153c3ef769aea3d81bdc4e2
孙源辉 <sunyuanhui@wisonic.cn>
2020-02-09 14:37:41 +0800

    完成 21. 合并两个有序链表

--- js/21. Merge Two Sorted Lists/MergeTwoSortedLists.js
+++ js/21. Merge Two Sorted Lists/MergeTwoSortedLists.js
@@ -10,7 +10,31 @@
  * @param {ListNode} l2
  * @return {ListNode}
  */
+const { ListNode } = require('../utils');
 var mergeTwoLists = function(l1, l2) {
-    
+    let list = new ListNode();
+    let head = list;
+    while(l1 || l2) {
+        if (l1 && !l2) {
+            list.next = new ListNode(l1.val);
+            l1 = l1.next;
+            list = list.next;
+        } else if (!l1 && l2) {
+            list.next = new ListNode(l2.val);
+            l2 = l2.next;
+            list = list.next;
+        } else {
+            if (l1.val < l2.val) {
+                list.next = new ListNode(l1.val);
+                l1 = l1.next;
+                list = list.next;
+            } else {
+                list.next = new ListNode(l2.val);
+                l2 = l2.next;
+                list = list.next;
+            }
+        }
+    }
+    return head.next;
 };
 module.exports = mergeTwoLists;
--- js/21. Merge Two Sorted Lists/MergeTwoSortedLists.test.js
+++ js/21. Merge Two Sorted Lists/MergeTwoSortedLists.test.js
@@ -4,6 +4,12 @@ const { ArrayToList, ListToArray } = require('../utils');
 test('merge two sorted lists 1', () => {
   const l1 = ArrayToList([1, 2, 4]);
   const l2 = ArrayToList([1, 3, 4]);
-  expect(ListToArray(funcmergeTwoLists(l1, l2))).toStrictEqual([1, 1, 2, 3, 4, 4]);
+  expect(ListToArray(mergeTwoLists(l1, l2))).toStrictEqual([1, 1, 2, 3, 4, 4]);
+});
+
+test('merge two sorted lists 2', () => {
+  const l1 = ArrayToList([1, 2, 4]);
+  const l2 = ArrayToList([5]);
+  expect(ListToArray(mergeTwoLists(l1, l2))).toStrictEqual([1, 2, 4, 5]);
 });
 
