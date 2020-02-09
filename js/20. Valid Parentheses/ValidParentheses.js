d58138a80c2734dafaa9b0a3738334ef8af242b3
孙源辉 <sunyuanhui@wisonic.cn>
2020-02-09 14:04:29 +0800

    完成 20. 有效的括号

---
+++ js/20. Valid Parentheses/ValidParentheses.js
@@ -0,0 +1,21 @@
+﻿/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isValid = function(s) {
+    let parentheses = { ')': '(', ']': '[', '}': '{' }
+    let stack = [];
+    for (let i = 0; i < s.length; i++) {
+        let top = stack.pop();
+        if (top === undefined) {
+            stack.push(s[i]);
+        } else {
+            if (parentheses[s[i]] !== top) {
+                stack.push(top);
+                stack.push(s[i]);
+            }
+        }
+    }
+    return stack.length === 0;
+};
+module.exports = isValid;
---
+++ js/20. Valid Parentheses/ValidParentheses.test.js
@@ -0,0 +1,22 @@
+﻿const isValid = require('./ValidParentheses');
+
+test('valid parentheses 1', () => {
+  expect(isValid("()")).toBe(true);
+});
+
+test('valid parentheses 2', () => {
+  expect(isValid("()[]{}")).toBe(true);
+});
+
+test('valid parentheses 3', () => {
+  expect(isValid("(]")).toBe(false);
+});
+
+test('valid parentheses 4', () => {
+  expect(isValid("([)]")).toBe(false);
+});
+
+test('valid parentheses 5', () => {
+  expect(isValid("{[]}")).toBe(true);
+});
+
