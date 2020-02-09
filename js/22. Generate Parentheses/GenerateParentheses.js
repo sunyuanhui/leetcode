ff1cdbb54b2546dbaa081cd9f42a3579229f875a
孙源辉 <sunyuanhui@wisonic.cn>
2020-02-10 00:03:15 +0800

    提交 22. 括号生成，递归逻辑还有问题，有不合法的括号

---
+++ js/22. Generate Parentheses/GenerateParentheses.js
@@ -0,0 +1,13 @@
+﻿/**
+ * @param {number} n
+ * @return {string[]}
+ */
+var generateParenthesis = function(n) {
+    return generate(n - 1, n, '(');
+};
+var generate = function(left, right, s) {
+    let s1 = (left !== 0) ? generate(left - 1, right, s + '(') : [s];
+    let s2 = (right !== 0) ? generate(left, right - 1, s + ')') : [s];
+    return [...s1, ...s2];
+};
+module.exports = generateParenthesis;
---
+++ js/22. Generate Parentheses/GenerateParentheses.test.js
@@ -0,0 +1,6 @@
+﻿const generateParenthesis = require('./GenerateParentheses');
+
+test('generate parentheses 1', () => {
+  expect(generateParenthesis(3)).toStrictEqual([ "((()))", "(()())", "(())()", "()(())", "()()()" ]);
+});
+
