7026fa16ab09446326e2aa5c0efb33532cf1ebbc
孙源辉 <sunyuanhui@wisonic.cn>
2020-02-10 21:45:50 +0800

    完成 23. 合并K个排序链表，但是效率不是最优

---
+++ js/23. Merge k Sorted Lists/MergekSortedLists.js
@@ -0,0 +1,47 @@
+﻿/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode[]} lists
+ * @return {ListNode}
+ */
+const { ListNode } = require('../utils');
+var mergeKLists = function(lists) {
+    let list = new ListNode();
+    let head = list;
+    let length = 0;
+    for (let i = 0; i < lists.length; i++) {
+        let l = lists[i];
+        while (l) {
+            l = l.next;
+            length++;
+        }
+    }
+
+    while (length !== 0) {
+        let minList;
+        let minValue;
+        for (let i = 0; i < lists.length; i++) {
+            let l = lists[i];
+            if (l === null) {
+                continue;
+            }
+            if (minValue === undefined || l.val < minValue) {
+                minList = i;
+                minValue = l.val;
+            }
+        }
+        if (minValue !== undefined) {
+            list.next = new ListNode(minValue);
+            list = list.next;
+            lists[minList] = lists[minList].next;
+        }
+        length--;
+    }
+    return head.next;
+};
+module.exports = mergeKLists;
---
+++ js/23. Merge k Sorted Lists/MergekSortedLists.test.js
@@ -0,0 +1,25 @@
+﻿const mergeKLists = require('./MergekSortedLists');
+const { ArrayToList, ListToArray } = require('../utils');
+
+test('merge k sorted lists 1', () => {
+  const lists = [
+    ArrayToList([1, 4, 5]),
+    ArrayToList([1, 3, 4]),
+    ArrayToList([2, 6]),
+  ];
+  const list = [1, 1, 2, 3, 4, 4, 5, 6];
+  expect(ListToArray(mergeKLists(lists))).toStrictEqual(list);
+});
+
+test('merge k sorted lists 2', () => {
+  const lists = [];
+  const list = [];
+  expect(ListToArray(mergeKLists(lists))).toStrictEqual(list);
+});
+
+test('merge k sorted lists 3', () => {
+  const lists = [[]];
+  const list = [];
+  expect(ListToArray(mergeKLists(lists))).toStrictEqual(list);
+});
+
