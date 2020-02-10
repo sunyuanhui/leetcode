608fab8e14fa6c5e6badd2878cc6d309ca05da67
孙源辉 <sunyuanhui@wisonic.cn>
2020-02-10 17:29:07 +0800

    完成 22. 括号生成

--- js/22. Generate Parentheses/GenerateParentheses.js
+++ js/22. Generate Parentheses/GenerateParentheses.js
@@ -6,8 +6,8 @@ var generateParenthesis = function(n) {
     return generate(n - 1, n, '(');
 };
 var generate = function(left, right, s) {
-    let s1 = (left !== 0) ? generate(left - 1, right, s + '(') : [s];
-    let s2 = (right !== 0) ? generate(left, right - 1, s + ')') : [s];
+    let s1 = (left !== 0 && right !== 0) ? generate(left - 1, right, s + '(') : [];
+    let s2 = (right !== 0 && right > left) ? generate(left, right - 1, s + ')') : (left === 0) ? [s] : [];
     return [...s1, ...s2];
 };
 module.exports = generateParenthesis;
