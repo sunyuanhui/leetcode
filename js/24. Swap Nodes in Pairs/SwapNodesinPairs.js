c234e4bc6d5103fd1cd08f4a249242f7e4d86aca
孙源辉 <sunyuanhui@wisonic.cn>
2020-02-10 22:19:00 +0800

    添加 24. 两两交换链表中的节点，未实现

---
+++ js/24. Swap Nodes in Pairs/SwapNodesinPairs.js
@@ -0,0 +1,15 @@
+﻿/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var swapPairs = function(head) {
+    
+};
+module.exports = swapPairs;
---
+++ js/24. Swap Nodes in Pairs/SwapNodesinPairs.test.js
@@ -0,0 +1,8 @@
+﻿const swapPairs = require('./SwapNodesinPairs');
+const { ArrayToList, ListToArray } = require('../utils');
+
+test('swap nodes in pairs 1', () => {
+  const l = ArrayToList([1, 2, 3, 4]);
+  expect(ListToArray(swapPairs(l))).toStrictEqual([2, 1, 4, 3]);
+});
+
