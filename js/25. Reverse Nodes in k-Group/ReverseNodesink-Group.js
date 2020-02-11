b718350b868ae006b8ec3f5add7e54d7a2d692b9
孙源辉 <sunyuanhui@wisonic.cn>
2020-02-11 22:59:04 +0800

    提交 25. K 个一组翻转链表的实现，不过现在逻辑还不太对

---
+++ js/25. Reverse Nodes in k-Group/ReverseNodesink-Group.js
@@ -0,0 +1,41 @@
+﻿/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @param {number} k
+ * @return {ListNode}
+ */
+const { ListNode } = require('../utils');
+var reverseKGroup = function(head, k) {
+    let tmp = head;
+    let list = [];
+    for (let i = 0; i < k; i++) {
+        if (!tmp || tmp.next === null) {
+            return head;
+        }
+        list.push(tmp);
+        tmp = tmp.next;
+    }
+
+    for (let i = 0; i < Math.floor(k / 2); i++) {
+        let swap = k - 1 - i;
+        let next = list[i].next;
+        
+        list[i].next = list[swap].next;
+        if (i + 1 === swap) {
+            list[swap].next = list[i];
+        } else {
+            list[swap].next = next;
+            list[swap - 1].next = list[i];
+        }
+    }
+
+    list[0].next = reverseKGroup(tmp, k);
+    return list[k - 1];
+};
+module.exports = reverseKGroup;
---
+++ js/25. Reverse Nodes in k-Group/ReverseNodesink-Group.test.js
@@ -0,0 +1,27 @@
+﻿const reverseKGroup = require('./ReverseNodesink-Group');
+const { ArrayToList, ListToArray } = require('../utils');
+
+test('reverse nodes in k-group 1', () => {
+  const l = ArrayToList([1, 2, 3, 4, 5]);
+  const k = 2;
+  expect(ListToArray(reverseKGroup(l, k))).toStrictEqual([2, 1, 4, 3, 5]);
+});
+
+test('reverse nodes in k-group 2', () => {
+  const l = ArrayToList([1, 2, 3, 4, 5]);
+  const k = 3;
+  expect(ListToArray(reverseKGroup(l, k))).toStrictEqual([3, 2, 1, 4, 5]);
+});
+
+test('reverse nodes in k-group 3', () => {
+  const l = ArrayToList([1, 2, 3, 4, 5]);
+  const k = 4;
+  expect(ListToArray(reverseKGroup(l, k))).toStrictEqual([4, 3, 2, 1, 5]);
+});
+
+test('reverse nodes in k-group 3', () => {
+  const l = ArrayToList([1, 2, 3, 4, 5]);
+  const k = 5;
+  expect(ListToArray(reverseKGroup(l, k))).toStrictEqual([5, 4, 3, 2, 1]);
+});
+
